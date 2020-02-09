9718b2d5957383c0a3f75a9edfa3d227cb7c8762
Jarrell Houston <jhouston2882@yahoo.com>
2020-02-08 20:06:12 -0600

    Updates

---
+++ precache-manifest.8c9430a49ca3cdc49c4c866cb1e17a47.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68cb3532e092613fa16dfd136e034ad8",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "c004096ffcf983b93c1a",
+    "url": "/Clicky-Game/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "820e1ad45d51bb09cd5d",
+    "url": "/Clicky-Game/static/css/main.b613154f.chunk.css"
+  },
+  {
+    "revision": "c004096ffcf983b93c1a",
+    "url": "/Clicky-Game/static/js/2.85fce268.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/Clicky-Game/static/js/2.85fce268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820e1ad45d51bb09cd5d",
+    "url": "/Clicky-Game/static/js/main.56f5aa7e.chunk.js"
+  },
+  {
+    "revision": "331e595879dc7af9f8f0",
+    "url": "/Clicky-Game/static/js/runtime-main.e5d1295f.js"
+  },
+  {
+    "revision": "9774a6658dce9f09e57ae8eb8fe3d476",
+    "url": "/Clicky-Game/static/media/barbara.9774a665.png"
+  },
+  {
+    "revision": "b707c96d7afe7ad39ec5b10de5be0494",
+    "url": "/Clicky-Game/static/media/brian.b707c96d.jpg"
+  },
+  {
+    "revision": "71d548916b5e6764606d9b171c5842d5",
+    "url": "/Clicky-Game/static/media/chicken.71d54891.png"
+  },
+  {
+    "revision": "b814a6bfb048185ff5bffc890dac3476",
+    "url": "/Clicky-Game/static/media/cleveland.b814a6bf.jpg"
+  },
+  {
+    "revision": "a153f0b3a6cface176c4d647c6ae5fd7",
+    "url": "/Clicky-Game/static/media/joe.a153f0b3.png"
+  },
+  {
+    "revision": "5f5facfec4e06c3e129419f1caf7affb",
+    "url": "/Clicky-Game/static/media/lois.5f5facfe.jpg"
+  },
+  {
+    "revision": "613d11002812a1322e3b06f62bca2d5c",
+    "url": "/Clicky-Game/static/media/meg.613d1100.png"
+  },
+  {
+    "revision": "70e0e788c917cbb06d90ca8e570de34e",
+    "url": "/Clicky-Game/static/media/peter.70e0e788.png"
+  },
+  {
+    "revision": "0982c584e659a2c2be6fb6bc3408900e",
+    "url": "/Clicky-Game/static/media/quagmire.0982c584.png"
+  },
+  {
+    "revision": "0af02d679c3b75cc4c381ff37bca72b5",
+    "url": "/Clicky-Game/static/media/rupert.0af02d67.png"
+  },
+  {
+    "revision": "6dd194bfeeb48c5679874ed810b6a1ca",
+    "url": "/Clicky-Game/static/media/stewie.6dd194bf.png"
+  }
+]);
